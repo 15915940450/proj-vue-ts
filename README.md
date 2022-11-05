5dcfb7429f9aaaac035d5403bccf2553d1ea77a4
Louis <15915940450@139.com>
2022-11-05 11:53:43 +0800

    path 2d and object 3d

--- README.md
+++ README.md
@@ -25,3 +25,80 @@ Since TypeScript cannot handle type information for `.vue` imports, they are shi
 2. Reload the VS Code window by running `Developer: Reload Window` from the command palette.
 
 You can learn more about Take Over mode [here](https://github.com/johnsoncodehk/volar/discussions/471).
+
+
+===t1
+```
+import {onMounted} from 'vue'
+import * as THREE from 'three'
+// console.log(THREE.REVISION)
+
+
+let runTHREE=()=>{
+  /* 
+  要素：
+  camera+scene+geometry+material+mesh+renderer
+
+  renderer=scene+camera
+  scene=mesh+...
+  mesh=geometry+material
+  */
+  let aspect = (window.innerWidth * .8)/ (window.innerHeight * .4)
+  const camera=new THREE.PerspectiveCamera(70,aspect,1e-2,1e2)
+  camera.position.z=1
+  const scene=new THREE.Scene()
+  let size=.4
+  const geometry=new THREE.BoxGeometry(size,size,size)
+  const material=new THREE.MeshNormalMaterial()
+  const mesh=new THREE.Mesh(geometry,material)
+  scene.add(mesh)
+
+
+  const renderer=new THREE.WebGLRenderer({
+    antialias:true,
+  })
+  renderer.setSize(window.innerWidth*.8,window.innerHeight*.4)
+  renderer.setAnimationLoop(animation)
+
+  elTHREE.appendChild(renderer.domElement)
+
+
+
+  function animation(time:number){
+    mesh.rotation.x=time/2000
+    mesh.rotation.y=time/1000
+    renderer.render(scene,camera)
+  }
+}
+
+
+onMounted(()=>{
+  runTHREE()
+})
+```
+
+
+Path2D()
+```
+import {onMounted} from 'vue'
+
+onMounted(()=>{
+  let ctx=canvasMain.getContext('2d')
+  console.log(ctx)
+
+
+  ctx.save()
+  let d_SVGpath ='M11 4C11 1.79086 9.20914 0 7 0H0V48H7C9.20914 48 11 46.2091 11 44L11 4Z'
+  let thePath2D_1 = new Path2D(d_SVGpath)
+  ctx.fillStyle ='#FFBB00'
+  ctx.fill(thePath2D_1)
+
+  let thePath2D_2 = new Path2D('M5.5 30V17')
+  ctx.strokeStyle='white'
+  ctx.lineWidth=2
+  ctx.lineCap ='round'
+  ctx.lineJoin ='round'
+  ctx.stroke(thePath2D_2)
+  ctx.restore()
+})
+```
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,86 @@
 <script lang="ts" setup>
 import {onMounted} from 'vue'
-import * as THREE from 'three'
-// console.log(THREE.REVISION)
+import * as _3D from 'three'
 
 
-let runTHREE=()=>{
   /* 
   要素：
-  camera++++++++
+  camera+scene+geometry+material+mesh+object3d+group+renderer
+
+  renderer=scene+camera
+  scene=mesh+object3d+group...
+  mesh=geometry+material
   */
-  let aspect = (window.innerWidth * .8)/ (window.innerHeight * .4)
-  const camera=new THREE.PerspectiveCamera(70,aspect,1e-2,1e2)
-  camera.position.z=1
-  const scene=new THREE.Scene()
-  let size=.4
-  const geometry=new THREE.BoxGeometry(size,size,size)
-  const material=new THREE.MeshNormalMaterial()
-  const mesh=new THREE.Mesh(geometry,material)
-  scene.add(mesh)
-
-
-  const renderer=new THREE.WebGLRenderer({
+let runTHREE=()=>{
+  let w=800;
+  let h=600;
+  let renderer = new _3D.WebGLRenderer({
     antialias:true,
   })
-  renderer.setSize(window.innerWidth*.8,window.innerHeight*.4)
+  renderer.setSize(w,h)
+
+  let scene=new _3D.Scene()
+
+  let size=0.8
+  let geometry = new _3D.BoxGeometry(size, size, size)
+  let material=new _3D.MeshNormalMaterial()
+  let mesh=new _3D.Mesh(geometry,material)
+
+  let object3d=new _3D.Object3D()
+  object3d.add(mesh)
+
+
+  /* 
+  import { Object3D } from '../core/Object3D.js';
+
+  class Group extends Object3D {
+
+    constructor() {
+
+      super();
+
+      this.isGroup = true;
+
+      this.type = 'Group';
+
+    }
+
+  }
+
+  export { Group };
+  */
+  let group=new _3D.Group()
+  group.add(object3d)
+  scene.add(group)
+
+  let aspect=w/h
+  let camera=new _3D.PerspectiveCamera(75,aspect,1e-2,1e2)
+  camera.position.z=1
+  camera.position.x=1
+  camera.lookAt(0,0,0)
+
+  let animation = (msec:number) => { 
+    // camera.position.x += .001
+    mesh.rotation.x+=Math.random()*1e-2
+    mesh.rotation.y += Math.random() * 1e-2
+    mesh.rotation.z += Math.random() * 1e-2
+
+    mesh.position.x = Math.random() * 1e-1
+    mesh.position.y = Math.random() * 1e-1
+    mesh.position.z = Math.random() * 1e-1
+    renderer.render(scene, camera)
+  }
   renderer.setAnimationLoop(animation)
 
+
+  
+
+
   elTHREE.appendChild(renderer.domElement)
-
-
-
-  function animation(time:number){
-    mesh.rotation.x=time/2000
-    mesh.rotation.y=time/1000
-    renderer.render(scene,camera)
-  }
 }
-
-
 onMounted(()=>{
   runTHREE()
 })
-
 </script>
 <template>
 <div class="App">
@@ -53,11 +92,14 @@ onMounted(()=>{
 </template>
 
 
-Vite does provide built-in support for .scss, .sass, .less, .styl and .stylus files. There is no need to install Vite-specific plugins for them, but the corresponding pre-processor itself must be installed:
-# .scss and .sass
-npm add -D sass
+Vite does provide built-in support for .scss, .sass, .less, .styl and .stylus files. 
+There is no need to install Vite-specific plugins for them, 
+but the corresponding pre-processor itself must be installed:
+  # .scss and .sass
+  npm add -D sass
 <style lang="scss">
 .App{
-  border: 1px solid dimgray;
+  border: 10px solid dimgray;
 }
+
 </style>
